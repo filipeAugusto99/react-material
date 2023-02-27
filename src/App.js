e2536a920968685a5ba7f4c5bc89cff1fde631f0
Filipe Augusto <filipecordeiro213@gmail.com>
2023-02-27 09:29:34 -0300

    material design

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <TextField
+        required
+        id="standard-required"
+        label="Required"
+        defaultValue="Hello World"
+        variant="standard"
+      />
+      <Button variant="contained">
+        Hello World!!
+      </Button>
+    </>
   );
 }
 
